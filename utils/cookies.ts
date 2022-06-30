233d387d7af29b5685f2da55c5bd1f677b38e605
lichang <2217024156@qq.com>
2022-06-30 17:51:35 +0800

    feat: 暂存

--- utils/cookies.ts
+++ utils/cookies.ts
@@ -1,9 +1,5 @@
-import { serialize, CookieSerializeOptions } from 'cookie'
-import { NextApiResponse } from 'next'
-
-/**
- * This sets `cookie` using the `res` object
- */
+import { serialize, CookieSerializeOptions } from "cookie";
+import { NextApiResponse } from "next";
 
 export const setCookie = (
   res: NextApiResponse,
@@ -12,13 +8,12 @@ export const setCookie = (
   options: CookieSerializeOptions = {}
 ) => {
   const stringValue =
-    typeof value === 'object' ? 'j:' + JSON.stringify(value) : String(value)
+    typeof value === "object" ? "j:" + JSON.stringify(value) : String(value);
 
-  if ( 'maxAge' in options) {
-    // options.expires = new Date(Date.now() + options.maxAge)
-    // options.maxAge /= 1000
+  if ("maxAge" in options && options.maxAge) {
+    options.expires = new Date(Date.now() + options.maxAge);
+    options.maxAge /= 1000;
   }
 
-  res.setHeader('Set-Cookie', serialize(name, stringValue, options))
-}
-
+  res.setHeader("Set-Cookie", serialize(name, stringValue, options));
+};
